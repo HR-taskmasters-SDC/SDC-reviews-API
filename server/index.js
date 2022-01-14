2692682d7f12fbc442166e6bc7b4d5d84a7a7419
Weiran Cheng <chengwr1128@gmail.com>
2022-01-13 17:22:16 -0800

    Define the get request for all the reviews for certain product

--- server/index.js
+++ server/index.js
@@ -8,17 +8,38 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.get('/reviews/:product_id', (req, res) => {
-  const queryStr = 'SELECT * FROM reviews;'
+  const product_id = req.params.product_id;
+  const page = req.query.page || 1;
+  const count = req.query.count || 5;
+  const sort = req.query.sort || 'relevant';
+  let orderBy = 'ORDER BY date DESC, helpfulness DESC';
+
+  if ((sort).slice(1, sort.length - 1) === 'newest') {
+    orderBy = 'ORDER BY date DESC';
+  } else if ((sort).slice(1, sort.length - 1) === 'helpful') {
+    orderBy = 'ORDER BY helpfulness DESC';
+  }
+
+  const queryStr = `SELECT * FROM reviews
+                    WHERE product_id = ${product_id}
+                    ${orderBy}
+                    LIMIT ${count}
+                    ;`
+
   db.query(queryStr, (err, result) => {
     if (err) {
       console.error(err);
     } else {
-      res.status(200).send('OK');
+      res.status(200).send(result);
     }
   })
 });
 
+// .get(
+//   `${Options.URL}/reviews/?product_id=${props.selected.id}&count=999`,
+
 // app.get('/reviews/meta')
+// .get(`${Options.URL}/reviews/meta/?product_id=${props.selected.id}`, {
 
 app.post('/reviews/:product_id', (req, res) => {
 
@@ -33,6 +54,21 @@ app.post('/reviews/:product_id', (req, res) => {
   })
 });
 
+// .post(
+//   `${Options.URL}/reviews`,
+//   {
+//     product_id: props.selected.id,
+//     rating: rating,
+//     summary: summary,
+//     body: body,
+//     recommend: recommend,
+//     name: nickname,
+//     email: email,
+//     photos: photos,
+//     characteristics: characteristics,
+//   },
+
+
 app.put('/reviews/:review_id/helpful', (req, res) => {
   const queryStr = ';'
   db.query(queryStr, params, (err, result) => {
@@ -44,6 +80,14 @@ app.put('/reviews/:review_id/helpful', (req, res) => {
   })
 });
 
+// axios
+//         .put(`${Options.URL}/reviews/${props.review.review_id}/helpful`, null, {
+//           headers: {
+//             Authorization: Options.TOKEN,
+//           },
+
+
+
 app.put('/reviews/:review_id/report', (req, res) => {
   const queryStr = ';'
   db.query(queryStr, params, (err, result) => {
@@ -54,5 +98,8 @@ app.put('/reviews/:review_id/report', (req, res) => {
     }
   })
 });
+
+
+//.put(`${Options.URL}/reviews/${props.review.review_id}/report`, null,
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
